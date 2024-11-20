02919d1e530feb436dd6350d245a143cd2ec49ee
HareuBang <divjae7124@gmail.com>
2024-11-20 10:01:24 +0900

    feat: 1_BookCard 추가

---
+++ src/components/BookCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./BookCard.css";
+
+const BookCard = ({ book }) => {
+  return (
+    <div className="book-card">
+      <Link to={`/book/${book.id}`} className="book-link">
+        <img src={book.image} alt={book.title} className="book-image" />
+        <h3 className="book-title">{book.title}</h3>
+        <div className="book-info">
+          <span className="category">{book.category}</span>
+          <span className="condition">{book.condition}</span>
+        </div>
+        <div className="book-options">
+          {book.forSale && <span className="option sale">판매</span>}
+          {book.forExchange && <span className="option exchange">교환</span>}
+        </div>
+        <div className="book-price">{book.price}원</div>
+        <button className="add-to-cart">Add to Cart</button>
+      </Link>
+    </div>
+  );
+};
+
+export default BookCard;
