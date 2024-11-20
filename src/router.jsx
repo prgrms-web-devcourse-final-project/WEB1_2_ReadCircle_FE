75a00358cd04581007b85eb166be01b3b9b74795
HareuBang <divjae7124@gmail.com>
2024-11-20 10:13:24 +0900

    style: 1_BookList SCSS 추가

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,13 @@
 import LoginPage from "./pages/LoginPage";
 import JoinPage from "./pages/JoinPage";
 import MainPage from "./pages/MainPage";
+import BookList from "./components/BookList";
 
 const routes = [
   { path: "/", element: <MainPage /> },
   { path: "/join", element: <JoinPage /> },
   { path: "/login", element: <LoginPage /> },
+  { path: "/BookList", element: <BookList /> },
 ];
 
 export default routes;
