a99add6ce3cd3146ca3f6a668c538571154030a9
HareuBang <divjae7124@gmail.com>
2024-11-20 12:27:35 +0900

    docs: MainPage 분리된 코드 적용

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,21 +1,11 @@
-import React, { useState } from "react";
-import { IoBookOutline } from "react-icons/io5";
-import { FaDumbbell } from "react-icons/fa";
-import { FaMoneyBillTrendUp } from "react-icons/fa6";
-import { MdOutlineMan4 } from "react-icons/md";
-import { FaComputer } from "react-icons/fa6";
-import { FaRegCommentDots } from "react-icons/fa6";
+import React from "react";
 import { Link } from "react-router-dom";
 import "./../styles/scss/MainPage.scss";
 import Header from "../components/Header";
+import SearchBar from "../components/SearchBar";
+import FloatingButton from "../components/FloatingButton";
 
 const MainPage = () => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
-  const toggleModal = () => setModalOpen(!modalOpen);
-
   const categories = [
     {
       name: "소설/시",
@@ -53,136 +43,55 @@ const MainPage = () => {
     { title: "MARIE CURIE", price: "17,000원", image: "src/assets/book1.png" },
     { title: "MuhammadAli", price: "18,000원", image: "src/assets/book2.png" },
     { title: "MALCOLM X", price: "22,000원", image: "src/assets/book3.png" },
-    {
-      title: "MICHELLE OBAMA",
-      price: "21,000원",
-      image: "src/assets/book4.png",
-    },
-    { title: "WALT DISNEY", price: "19,000원", image: "src/assets/book5.png" },
-    { title: "MARIE CURIE", price: "17,000원", image: "src/assets/book1.png" },
-    { title: "MuhammadAli", price: "18,000원", image: "src/assets/book2.png" },
-    { title: "MALCOLM X", price: "22,000원", image: "src/assets/book3.png" },
-    {
-      title: "MICHELLE OBAMA",
-      price: "21,000원",
-      image: "src/assets/book4.png",
-    },
-    { title: "WALT DISNEY", price: "19,000원", image: "src/assets/book5.png" },
-    { title: "MARIE CURIE", price: "17,000원", image: "src/assets/book1.png" },
-    { title: "MuhammadAli", price: "18,000원", image: "src/assets/book2.png" },
-    { title: "MALCOLM X", price: "22,000원", image: "src/assets/book3.png" },
-    {
-      title: "MICHELLE OBAMA",
-      price: "21,000원",
-      image: "src/assets/book4.png",
-    },
-    { title: "WALT DISNEY", price: "19,000원", image: "src/assets/book5.png" },
-    { title: "MARIE CURIE", price: "17,000원", image: "src/assets/book1.png" },
-    { title: "MuhammadAli", price: "18,000원", image: "src/assets/book2.png" },
-    { title: "MALCOLM X", price: "22,000원", image: "src/assets/book3.png" },
-    {
-      title: "MICHELLE OBAMA",
-      price: "21,000원",
-      image: "src/assets/book4.png",
-    },
-    { title: "WALT DISNEY", price: "19,000원", image: "src/assets/book5.png" },
-    { title: "MARIE CURIE", price: "17,000원", image: "src/assets/book1.png" },
-    { title: "MuhammadAli", price: "18,000원", image: "src/assets/book2.png" },
-    { title: "MALCOLM X", price: "22,000원", image: "src/assets/book3.png" },
-    {
-      title: "MICHELLE OBAMA",
-      price: "21,000원",
-      image: "src/assets/book4.png",
-    },
-    { title: "WALT DISNEY", price: "19,000원", image: "src/assets/book5.png" },
   ];
 
   return (
     <>
       <Header />
       <div className="main">
-        <div className="main">
-          {/* 햄버거 + 검색창 */}
-          <div className="main__search">
-            <button
-              className="main__hamburger"
-              onClick={toggleDropdown}
-              style={{ fontWeight: "700" }}
-            >
-              ☰
-            </button>
-            <input type="text" placeholder="   Book name or writer name" />
-            {dropdownOpen && (
-              // 추후에 디자인 추가
-              <div className="main__dropdown">
-                <a href="#">소설 / 시</a>
-                <a href="#">자기계발</a>
-                <a href="#">경제경영</a>
-                <a href="#">인문학</a>
-                <a href="#">컴퓨터 / 모바일</a>
-              </div>
-            )}
-          </div>
-
-          <div className="main__banner">
-            <img src="src/assets/banner.png" alt="Banner" />
-          </div>
-
-          <div className="main__categories">
-            <h2 className="main__categories-title">카테고리</h2>
-            <div className="main__categories-list">
-              {categories.map((category, index) => (
-                <Link
-                  key={index}
-                  to={category.link}
-                  className="category"
-                  style={{ textDecoration: "none" }}
+        <SearchBar />
+        <div className="main__banner">
+          <img src="src/assets/banner.png" alt="Banner" />
+        </div>
+        <div className="main__categories">
+          <h2 className="main__categories-title">카테고리</h2>
+          <div className="main__categories-list">
+            {categories.map((category, index) => (
+              <Link
+                key={index}
+                to={category.link}
+                className="category"
+                style={{ textDecoration: "none" }}
+              >
+                <div
+                  className="category__box"
+                  style={{ backgroundColor: category.color }}
                 >
-                  <div
-                    className="category__box"
-                    style={{ backgroundColor: category.color }}
+                  <span
+                    className="category__icon"
+                    style={{ color: "white", fontSize: "36px" }}
                   >
-                    <span
-                      className="category__icon"
-                      style={{ color: "white", fontSize: "36px" }}
-                    >
-                      {category.icon}
-                    </span>
-                  </div>
-                  <span className="category__name">{category.name}</span>
-                </Link>
-              ))}
-            </div>
-          </div>
-
-          <h2 className="main__title">새로 등록된 상품</h2>
-          <div className="main__products">
-            {products.map((product, index) => (
-              <div key={index} className="product">
-                <img src={product.image} alt={product.title} />
-                <div className="product__info">
-                  <h3>{product.title}</h3>
-                  <p>{product.price}</p>
+                    {category.icon}
+                  </span>
                 </div>
-              </div>
+                <span className="category__name">{category.name}</span>
+              </Link>
             ))}
           </div>
-
-          <button className="main__floating-button" onClick={toggleModal}>
-            {modalOpen ? "X" : <FaRegCommentDots />}
-          </button>
-
-          {/* 추후에 디자인 추가 */}
-          {modalOpen && (
-            <div className="modal">
-              <div className="modal__content">
-                <h3>홍길동</h3>
-                <p>"홍길동"님이 채팅을 요청합니다.</p>
-                <button>채팅</button>
+        </div>
+        <h2 className="main__title">새로 등록된 상품</h2>
+        <div className="main__products">
+          {products.map((product, index) => (
+            <div key={index} className="product">
+              <img src={product.image} alt={product.title} />
+              <div className="product__info">
+                <h3>{product.title}</h3>
+                <p>{product.price}</p>
               </div>
             </div>
-          )}
+          ))}
         </div>
+        <FloatingButton />
       </div>
     </>
   );
