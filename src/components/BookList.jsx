34f6acabeb06c95cd604c65f3783f8a1942008ae
HareuBang <divjae7124@gmail.com>
2024-11-20 10:22:02 +0900

    test: test 데이터 추가 및 수정

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import BookCard from "./BookCard";
 import "../styles/scss/BookList.scss";
+import Header from "./Header";
 
 const BookList = () => {
   const books = [
     {
       id: 1,
       title: "Psychology of Money",
-      image: "https://via.placeholder.com/150",
+      image: "src/assets/book1.png",
       category: "Novel",
       condition: "최상",
       forSale: true,
@@ -17,7 +18,137 @@ const BookList = () => {
     {
       id: 2,
       title: "How Innovation Works",
-      image: "https://via.placeholder.com/150",
+      image: "src/assets/book2.png",
+      category: "Novel",
+      condition: "최상",
+      forSale: true,
+      forExchange: true,
+      price: 15000,
+    },
+    {
+      id: 2,
+      title: "How Innovation Works",
+      image: "src/assets/book3.png",
+      category: "Novel",
+      condition: "최상",
+      forSale: false,
+      forExchange: true,
+      price: 15000,
+    },
+    {
+      id: 2,
+      title: "How Innovation Works",
+      image: "src/assets/book4.png",
+      category: "Novel",
+      condition: "최상",
+      forSale: true,
+      forExchange: false,
+      price: 15000,
+    },
+    {
+      id: 2,
+      title: "How Innovation Works",
+      image: "src/assets/book5.png",
+      category: "Novel",
+      condition: "최상",
+      forSale: true,
+      forExchange: true,
+      price: 15000,
+    },
+    {
+      id: 1,
+      title: "Psychology of Money",
+      image: "src/assets/book1.png",
+      category: "Novel",
+      condition: "최상",
+      forSale: true,
+      forExchange: true,
+      price: 17000,
+    },
+    {
+      id: 2,
+      title: "How Innovation Works",
+      image: "src/assets/book2.png",
+      category: "Novel",
+      condition: "최상",
+      forSale: true,
+      forExchange: false,
+      price: 15000,
+    },
+    {
+      id: 2,
+      title: "How Innovation Works",
+      image: "src/assets/book3.png",
+      category: "Novel",
+      condition: "최상",
+      forSale: true,
+      forExchange: false,
+      price: 15000,
+    },
+    {
+      id: 2,
+      title: "How Innovation Works",
+      image: "src/assets/book4.png",
+      category: "Novel",
+      condition: "최상",
+      forSale: true,
+      forExchange: false,
+      price: 15000,
+    },
+    {
+      id: 2,
+      title: "How Innovation Works",
+      image: "src/assets/book5.png",
+      category: "Novel",
+      condition: "최상",
+      forSale: true,
+      forExchange: false,
+      price: 15000,
+    },
+    {
+      id: 1,
+      title: "Psychology of Money",
+      image: "src/assets/book1.png",
+      category: "Novel",
+      condition: "최상",
+      forSale: true,
+      forExchange: true,
+      price: 17000,
+    },
+    {
+      id: 2,
+      title: "How Innovation Works",
+      image: "src/assets/book2.png",
+      category: "Novel",
+      condition: "최상",
+      forSale: true,
+      forExchange: false,
+      price: 15000,
+    },
+    {
+      id: 2,
+      title: "How Innovation Works",
+      image: "src/assets/book3.png",
+      category: "Novel",
+      condition: "최상",
+      forSale: true,
+      forExchange: false,
+      price: 15000,
+    },
+    {
+      id: 2,
+      title: "How Innovation Works",
+      image: "src/assets/book4.png",
+      category: "Novel",
+      condition: "최상",
+      forSale: true,
+      forExchange: false,
+      price: 15000,
+    },
+    {
+      id: 2,
+      title: "How Innovation Works",
+      image: "src/assets/book5.png",
       category: "Novel",
       condition: "최상",
       forSale: true,
@@ -27,11 +158,14 @@ const BookList = () => {
   ];
 
   return (
-    <div className="book-list">
-      {books.map((book) => (
-        <BookCard key={book.id} book={book} />
-      ))}
-    </div>
+    <>
+      <Header />
+      <div className="book-list">
+        {books.map((book) => (
+          <BookCard key={book.id} book={book} />
+        ))}
+      </div>
+    </>
   );
 };
 
