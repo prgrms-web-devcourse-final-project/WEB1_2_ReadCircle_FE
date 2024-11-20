ef20f42173a87a73b3bb86c8992503e6be43187a
HareuBang <divjae7124@gmail.com>
2024-11-20 09:13:23 +0900

    feat: Header 추가

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Header.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Link to="/" className="header__logo">
+        ReadCircle
+      </Link>
+      <nav className="header__nav">
+        <Link to="/new">New</Link>
+        <Link to="/sell">Sell</Link>
+        <Link to="/buy">Buy</Link>
+        <Link to="/exchange">Exchange</Link>
+        <Link to="/mypage">Mypage</Link>
+        <Link to="/login" className="header__login" style={{ color: "#fff" }}>
+          Login
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
