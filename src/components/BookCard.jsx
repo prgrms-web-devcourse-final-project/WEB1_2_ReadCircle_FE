cf8238e196d8d190a9f2f44e9c59d230a8fd215a
HareuBang <divjae7124@gmail.com>
2024-11-20 10:15:46 +0900

    docs: BookCard css -> scss import 수정

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./BookCard.css";
+import "../styles/scss/BookCard.scss";
 
 const BookCard = ({ book }) => {
   return (
