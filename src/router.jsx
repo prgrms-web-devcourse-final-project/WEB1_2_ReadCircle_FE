22deba53b21bf86c85261423a8a896e8bee01c20
이영주 <lllucia@iyeongjuui-MacBookAir.local>
2024-11-22 12:27:31 +0900

    헤더 추가 및 css 수정

--- src/router.jsx
+++ src/router.jsx
@@ -2,12 +2,18 @@ import LoginPage from "./pages/LoginPage";
 import JoinPage from "./pages/JoinPage";
 import MainPage from "./pages/MainPage";
 import BookListPage from "./pages/BookListPage";
+import PostCreatePage from "./pages/PostCreatePage";
+import PostViewPage from "./pages/PostViewPage";
+import PostEditPage from "./pages/PostEditPage";
 
 const routes = [
   { path: "/", element: <MainPage /> },
   { path: "/join", element: <JoinPage /> },
   { path: "/login", element: <LoginPage /> },
   { path: "/BookList", element: <BookListPage /> },
+  { path: "/create", element: <PostCreatePage /> },
+  { path: "/view", element: <PostViewPage /> },
+  { path: "/edit", element: <PostEditPage /> },
 ];
 
 export default routes;
