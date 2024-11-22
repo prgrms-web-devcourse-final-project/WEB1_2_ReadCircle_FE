6a4063fdcbe5c2eae405dbb7db1ba9f1be41f221
JunHee Roh <122170091+shwnsl@users.noreply.github.com>
2024-11-22 10:37:42 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
@@ -19,4 +8,3 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
->>>>>>> d43711b325b6b88c5c9517e0e43db772ab10f7d5
