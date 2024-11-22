b744dc13466478f9662df5432b3c6f9bca759e28
audskd <audskd@gmail.com>
2024-11-22 11:09:13 +0900

    css 수정

--- src/router.jsx
+++ src/router.jsx
@@ -1,16 +1,6 @@
 import LoginPage from "./pages/LoginPage";
 import JoinPage from "./pages/JoinPage";
 import MainPage from "./pages/MainPage";
-<<<<<<< HEAD
-
-const routes = [
-    { path: '/', element: <MainPage /> },
-    { path: '/join', element: <JoinPage /> },
-    { path: '/login', element: <LoginPage /> }
-]
-
-export default routes;
-=======
 import BookListPage from "./pages/BookListPage";
 
 const routes = [
@@ -21,4 +11,3 @@ const routes = [
 ];
 
 export default routes;
->>>>>>> d43711b325b6b88c5c9517e0e43db772ab10f7d5
