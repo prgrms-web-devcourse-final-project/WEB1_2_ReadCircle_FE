95965e60c59ceb176691f48204ab85449c7a0439
HareuBang <divjae7124@gmail.com>
2024-11-29 21:53:15 +0900

    chore: api.js 롤백

--- src/api.js
+++ src/api.js
@@ -1,21 +1,10 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "http://localhost:5000", // Express 서버 URL
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// export default api;
-
-// 이커머스, 직거래 게시글 조회 API
 import axios from "axios";
 
-const API_BASE_URL = "/api"; // 기본 API URL
+const api = axios.create({
+  baseURL: "http://localhost:5000", // Express 서버 URL
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-export const fetchBooks = (query = "") =>
-  axios.get(`${API_BASE_URL}/books`, { params: { query } });
-
-export const fetchDirectBooks = (query = "") =>
-  axios.get(`${API_BASE_URL}/posts`, { params: { query } });
+export default api;
