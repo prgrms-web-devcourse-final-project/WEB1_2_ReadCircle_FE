4c7c616f89e39a327c56c1de306798f5825d2b60
HareuBang <divjae7124@gmail.com>
2024-11-30 00:58:26 +0900

    style: router.jsx 정리

--- src/router.jsx
+++ src/router.jsx
@@ -1,21 +1,23 @@
 import LoginPage from "./pages/LoginPage";
 import JoinPage from "./pages/JoinPage";
-import MainPage from "./pages/MainPage";
-import BookListPage from "./pages/BookListPage";
 import MyPage from "./pages/MyPage";
 import PostCreatePage from "./pages/PostCreatePage";
 import PostViewPage from "./pages/PostViewPage";
 import PostEditPage from "./pages/PostEditPage";
+import MainPage from "./pages/MainPage";
+import BookListPage from "./pages/BookListPage";
 
 const routes = [
-  { path: "/", element: <MainPage /> },
   { path: "/join", element: <JoinPage /> },
   { path: "/login", element: <LoginPage /> },
-  { path: "/BookList", element: <BookListPage /> },
   { path: "/mypage", element: <MyPage /> },
   { path: "/create", element: <PostCreatePage /> },
   { path: "/view", element: <PostViewPage /> },
   { path: "/edit", element: <PostEditPage /> },
+
+  // HareuBang
+  { path: "/", element: <MainPage /> },
+  { path: "/BookList", element: <BookListPage /> },
 ];
 
 export default routes;
