8404e815158df61c89a515952d2765ddaba1138e
HareuBang <divjae7124@gmail.com>
2024-11-30 04:10:37 +0900

    feat: 통합 게시물 카드 추가

---
+++ src/components/SearchResultCard.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/scss/SearchResultCard.scss";
+
+const SearchResultCard = ({ book }) => {
+  return (
+    <div className="book-card__int">
+      <Link to={`/searchmall/${book.ISBN}`}>
+        <div className="book-category">
+          <span>{book.category}</span>
+        </div>
+        <div className="book-image">
+          <img src={book.thumbnailUrl} alt={book.title} />
+        </div>
+        <div className="book-details">
+          <h3 className="book-isbn">ISBN: {book.ISBN}</h3>
+          <h3 className="book-title">{book.title}</h3>
+          <p className="book-author">by {book.author}</p>
+          <p className="book-year">Published: {book.publishDate}</p>
+          <p className="book-publisher">publisher: {book.publisher}</p>
+          <div className="stock-info">
+            <p>New: {book.newCount} copies</p>
+            <p>Used: {book.usedCount} copies</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default SearchResultCard;
+
+// 찜(관심) 관련 코드 [보류]
+// import { FaRegBookmark } from "react-icons/fa";
+// import { FaBookmark } from "react-icons/fa";
+// {isFavorite ? <FaBookmark /> : <FaRegBookmark />}
