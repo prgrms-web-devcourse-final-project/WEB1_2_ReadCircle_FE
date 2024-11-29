18f1435cb0df276212bce344e4e23f41a633e113
audskd <audskd@gmail.com>
2024-11-29 14:44:52 +0900

    회원가입/로그인/마이페이지/내 정보수정 기능 완료

--- src/router.jsx
+++ src/router.jsx
@@ -2,12 +2,14 @@ import LoginPage from "./pages/LoginPage";
 import JoinPage from "./pages/JoinPage";
 import MainPage from "./pages/MainPage";
 import BookListPage from "./pages/BookListPage";
+import MyPage from "./pages/MyPage";
 
 const routes = [
   { path: "/", element: <MainPage /> },
   { path: "/join", element: <JoinPage /> },
   { path: "/login", element: <LoginPage /> },
   { path: "/BookList", element: <BookListPage /> },
+  { path: "/mypage", element: <MyPage /> },
 ];
 
 export default routes;
