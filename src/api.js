648d381a8908f8a78dd0114a1fdaa8fd7d47004e
HareuBang <divjae7124@gmail.com>
2024-11-29 20:43:08 +0900

    build: api 호출 로직 분리

--- src/api.js
+++ src/api.js
@@ -1,10 +1,21 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:5000", // Express 서버 URL
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default api;
+
+// 이커머스, 직거래 게시글 조회 API
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:5000", // Express 서버 URL
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+const API_BASE_URL = "/api"; // 기본 API URL
 
-export default api;
+export const fetchBooks = (query = "") =>
+  axios.get(`${API_BASE_URL}/books`, { params: { query } });
+
+export const fetchDirectBooks = (query = "") =>
+  axios.get(`${API_BASE_URL}/posts`, { params: { query } });
