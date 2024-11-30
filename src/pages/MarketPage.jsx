2cddcd51a53416c108088941ce5d6ecd47de151d
HareuBang <divjae7124@gmail.com>
2024-11-30 23:41:19 +0900

    feat: 직거래 상세 조회 페이지 경로 롤백

--- src/pages/MarketPage.jsx
+++ src/pages/MarketPage.jsx
@@ -143,7 +143,7 @@ const Market = () => {
             books={filteredBooks}
             onCardClick={(bookId) =>
               // 직거래 책 상세 페이지 경로
-              navigate(`/view/${id}`)
+              navigate(`/view`)
             }
             showDeliveryFee={false}
             showActions={false}
