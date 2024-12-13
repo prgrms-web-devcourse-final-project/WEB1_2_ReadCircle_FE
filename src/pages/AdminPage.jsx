401966d09ce697b6a2213d9d136e75844b7aa525
HareuBang <divjae7124@gmail.com>
2024-12-13 15:35:30 +0900

    feat: 관리자 페이지 Header에 추가

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -2,36 +2,40 @@ import React, { useState } from "react";
 import SaleManagement from "../components/SaleManagement";
 import UserList from "../components/UserList";
 import "../styles/scss/AdminPage.scss";
+import Header from "../components/Header";
 
 const AdminPage = () => {
   const [activeTab, setActiveTab] = useState("user"); // 기본 탭 설정
 
   return (
-    <div className="admin-page">
-      <h1>관리자 페이지</h1>
+    <>
+      <Header />
+      <div className="admin-page">
+        <h1>관리자 페이지</h1>
 
-      {/* 탭 메뉴 */}
-      <div className="tab-menu">
-        <button
-          className={activeTab === "user" ? "active" : ""}
-          onClick={() => setActiveTab("user")}
-        >
-          회원 관리
-        </button>
-        <button
-          className={activeTab === "sale" ? "active" : ""}
-          onClick={() => setActiveTab("sale")}
-        >
-          판매 관리
-        </button>
-      </div>
+        {/* 탭 메뉴 */}
+        <div className="tab-menu">
+          <button
+            className={activeTab === "user" ? "active" : ""}
+            onClick={() => setActiveTab("user")}
+          >
+            회원 관리
+          </button>
+          <button
+            className={activeTab === "sale" ? "active" : ""}
+            onClick={() => setActiveTab("sale")}
+          >
+            판매 관리
+          </button>
+        </div>
 
-      {/* 선택된 탭에 따른 컴포넌트 렌더링 */}
-      <div className="tab-content">
-        {activeTab === "user" && <UserList />}
-        {activeTab === "sale" && <SaleManagement />}
+        {/* 선택된 탭에 따른 컴포넌트 렌더링 */}
+        <div className="tab-content">
+          {activeTab === "user" && <UserList />}
+          {activeTab === "sale" && <SaleManagement />}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
